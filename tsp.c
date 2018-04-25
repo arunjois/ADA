57fa7db372ee792ad7b9195a33efdd642e6e5284
Death Killer <death@killer.com>
2018-04-25 14:04:36 +0530

    Added TSP Files

--- tsp.c
+++ tsp.c
@@ -1,99 +1,48 @@
 #include<stdio.h>
-#define INFINITY 9999
-#define MAX 10
-int a[10][10], t[10][2], k=0, apxcost=0;
-void dfs(int cov[], int s, int n)
+#include<stdlib.h>
+int c=0,cost=999,graph[20][20];
+int a[20];
+void swap(int *x,int *y)
 {
-	int i;
-	cov[s]=1;
-	for(i=0; i<=n; i++)
-	{
-		if(!cov[i] && a[s][i])
-		{
-			t[k][0]=s;
-			t[k][1]=i;
-			k++;
-			dfs(cov,i,n);
-		}
-	}
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
 }
-void approximate(int cov[], int s, int n)
+void copy_array(int *a,int n)
 {
-	int i, sum=0;
-	dfs(cov, s, n);
-	if (a[t[k-1][1]][s]!=0)
-	{
-		t[k][0]=t[k-1][1];
-		t[k][1]=s;
-		k++;
-	}
-	if(t[k-1][1]==s)
-	{
-		printf("\nHamiltonian Circuit\n Vertices are:\n");
-		for(i=0; i<k; i++)
-		{
-			printf("%d -->", t[i][0]);
-			apxcost+=a[t[i][0]][t[i][1]];
-		}
-	}
-	if(apxcost>0)
-		printf("\n Approximatation Cost is %d \n", apxcost);
+	int sum=0,i;
+	for(i=0;i<=n;i++)
+		sum=sum+graph[a[i%n]][a[i+1]%n];
+	if(cost>sum)
+		cost=sum;
 }
-int tsp_dp(int c[][MAX], int tour[], int start, int n)
+void permutate(int *a,int i,int n)
 {
-	int i, j, k;
-	int temp[MAX], mintour[MAX], mincost=0, ccost;
-	if(start==n-2)
-		return c[tour[n-2]][tour[n-1]]+c[tour[n-1]][0];
-	mincost=INFINITY;
-	for(i=start+1; i<n; i++)
+	int j;
+	if(i==n)
+		copy_array(a,n);
+	else
 	{
-		//
-		printf("\nStart : %d", start);
-		for(j=0; j<n; j++)
-			temp[j]=tour[j];
-		temp[start+1]=tour[i];
-		temp[i]=tour[start+1];
-		if(c[tour[start]][tour[i]]+(ccost=tsp_dp(c, temp, start+1, n))<mincost)
+		for(j=1;j<=n;j++)
 		{
-			mincost=c[tour[start]][tour[i]]+ccost;
-			for(k=0; k<n; k++)
-				mintour[k]=temp[k];
+			swap((a+i),(a+j));
+			permutate(a,i+1,n);
+			swap(a+i,a+j);
 		}
 	}
-	for(i=0; i<n; i++)
-	{
-		tour[i]=mintour[i];
-	}
-	return mincost;
 }
 void main()
 {
-	int i, j, mincost, cost, n, s, cov[10], tour[10];
-	printf("\nEnter the total number of vertices:");
-	scanf("%d", &n);
-	printf("\n Enter the matrix for %d vertices\n", n);
-	for(i=0; i<n; i++)
-	{
-		for(j=0; j<n; j++)
-		{
-			scanf("\n%d", &a[i][j]);
-		}
-	}
-	for(i=0; i<n; i++)
-	{
-		cov[i]=0;
-		tour[i]=i;
-	}
-	approximate(cov,0,n);
-	mincost=tsp_dp(a, tour, 0, n);
-	for(i=0; i<n; i++)
-		printf("%d---->", tour[i]);
-	if(mincost>0)
-	{
-		printf("\n Dynamic Programming Min Cost is %d\n",mincost);
-		printf("\nError rate is %f\n", (((float)apxcost-mincost)/mincost)*100);
-		printf("\nAccuracy rate is %f\n",((float)apxcost/mincost));
-	}
+	int i,j,n;
+	printf("Enter the total no. of vertices:");
+	scanf("%d",&n);
+	printf("\n Enter Matrix of %d vertices\n",n);
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			scanf("%d",&graph[i][j]);
+	for(j=0;j<n;j++)
+		a[i]=1;
+	permutate(a,0,n-1);
+	printf("Minimum Cost:%d\n",cost);
 }
-
