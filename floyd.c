582b70642867f37bb09d1a4268d3e5b471b0f9cc
Death Killer <death@killer.com>
2018-04-27 15:32:41 +0530

    Floyd Indendted

--- floyd.c
+++ floyd.c
@@ -3,42 +3,42 @@
 int min(int,int);
 void floyd(int p[10][10],int n)
 {
-        int i,j,k;
-        for(k=1;k<=n;k++)
-        for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++)
-                if(i==j)
-                        p[i][j]=0;
-                else
-                        p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
+	int i,j,k;
+	for(k=1;k<=n;k++)
+		for(i=1;i<=n;i++)
+			for(j=1;j<=n;j++)
+				if(i==j)
+					p[i][j]=0;
+				else
+					p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
 }
 
 int main()
 {
-        int p[10][10],w,n,e,u,v,i,j;
-        printf("Enter no. of vertices:");
-        scanf("%d",&n);
-        for(i=1;i<=n;i++)
-        {
-                for(j=1;j<=n;j++)
-                        p[i][j]=999;
-        }
+	int p[10][10],w,n,e,u,v,i,j;
+	printf("Enter no. of vertices:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+			p[i][j]=999;
+	}
 	printf("Enter weight matrix\n");
-        for(i=1;i<=n;i++)
-        {
+	for(i=1;i<=n;i++)
+	{
 		for(j=1;j<=n;j++)
 		{
-                scanf("%d",&p[i][j]);
-        }
+			scanf("%d",&p[i][j]);
+		}
 	}
-        printf("\nMatrix of input data is\n");
-        for(i=1;i<=n;i++)
-        {
-                for(j=1;j<=n;j++)
-                        printf("%d\t",p[i][j]);
-                printf("\n");
-        }
-        floyd(p,n);
+	printf("\nMatrix of input data is\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+			printf("%d\t",p[i][j]);
+		printf("\n");
+	}
+	floyd(p,n);
 	printf("Transitive closure\n");
 	for(i=1;i<=n;i++)
 	{
@@ -46,7 +46,7 @@ int main()
 			printf("%d\t",p[i][j]);
 		printf("\n");	
 	}
-	
+
 	printf("\nThe short paths are\n");
 	for(i=1;i<=n;i++)
 		for(j=1;j<=n;j++)
@@ -59,8 +59,8 @@ int main()
 int min(int a,int b)
 {
 	if(a<b)
-	return (a);
+		return (a);
 	else
-	return(b);
+		return(b);
 }
 
